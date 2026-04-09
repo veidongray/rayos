9946d27a6c8a9be7e9d720659e95543ace88ebaf
veidongray <veidongray@qq.com>
2026-04-09 08:26:03 +0800

    New head.S

--- src/main.c
+++ src/main.c
@@ -8,13 +8,13 @@
 
 void kernel_main(void)
 {
-    // gdt_init();
-    // idt_init();
-    // page_init();
+    gdt_init();
+    idt_init();
+    page_init();
 
-    // cga_info("heap_top: 0x%X.\n", (uint32_t)heap_top);
-    // cga_info("Kernel initialized successfully.\n");
-    // cga_info("Systicks %uHZ.\n", TIMER_FREQ);
+    cga_info("heap_top: 0x%X.\n", (uint32_t)heap_top);
+    cga_info("Kernel initialized successfully.\n");
+    cga_info("Systicks %uHZ.\n", TIMER_FREQ);
     while (1)
     {
         asm volatile("hlt\r\n");
@@ -33,6 +33,6 @@ void main(uint32_t grub_magic, uint32_t mbi_addr)
         }
     }
     parse_multiboot2_mmap((void*)mbi_addr);
-    while (1);
-    // kernel_main();
+    while(1);
+    kernel_main();
 }
