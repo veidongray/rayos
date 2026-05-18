7e6df58ade4f4edc03d2e6a889d1b6ade6b250d8
veidongray <veidongray@qq.com>
2026-05-18 20:46:03 +0800

    Implement early page

--- inc/page.h
+++ inc/page.h
@@ -2,5 +2,6 @@
 #define PAGE_H
 
 void page_init(void);
+void load_pml4(uint64_t pml4_physaddr);
 
 #endif // PAGE_H
--- src/page.c
+++ src/page.c
@@ -1,61 +1,61 @@
 #include <stdint.h>
+#include "page.h"
 #include "multiboot2.h"
 
+#define KERNEL_BASE 0xFFFFFFFFC0000000ULL
 #define ALIGN_4K(val) (((uint64_t)(val) & ~0xfff) + 4096)
 
-static uint64_t *pml4;
-static uint64_t *pdpt;
-static uint64_t *pd;
+__attribute__((aligned(4096))) static uint64_t pml4[512];
+__attribute__((aligned(4096))) static uint64_t pdpt[512];
+__attribute__((aligned(4096))) static uint64_t pd[512];
 static uint64_t *pt;
 static uint64_t *kernel_virt_end_aligned;
 extern uint64_t _kernel_virt_end_aligned[];
 
 void page_init(void)
 {
+    uint64_t i;
     uint64_t total_pte;
     uint64_t total_pde;
-    uint64_t total_pdpte;
-    uint64_t total_pml4e;
 
     kernel_virt_end_aligned = (uint64_t *)_kernel_virt_end_aligned;
-    total_pte = get_total_mem() / 4096;
-    total_pde = (total_pte / 512) + 1;
-    total_pdpte = (total_pde / 512) + 1;
-    total_pml4e = (total_pdpte / 512) + 1;
+    if (get_total_mem() <= 128 * 1024 * 1024)
+    {
+        total_pte = (128 * 1024 * 1024) / 4096;
+        total_pde = (total_pte / 512);
 
-    pml4 = kernel_virt_end_aligned;
-    kernel_virt_end_aligned = kernel_virt_end_aligned + 512;
-
-    pdpt = kernel_virt_end_aligned;
-    if (total_pdpte <= 512)
-    {
-        kernel_virt_end_aligned = kernel_virt_end_aligned + 512;
-    }
-    else
-    {
-        kernel_virt_end_aligned = (uint64_t *)ALIGN_4K(kernel_virt_end_aligned + total_pdpte);
-    }
-
-    pd = kernel_virt_end_aligned;
-    if (total_pde <= 512)
-    {
-        kernel_virt_end_aligned = kernel_virt_end_aligned + 512;
-    }
-    else
-    {
-        kernel_virt_end_aligned = (uint64_t *)ALIGN_4K(kernel_virt_end_aligned + total_pde);
+        pml4[511] = ((uint64_t)pdpt - KERNEL_BASE) | 0x3ULL;
+        pdpt[511] = ((uint64_t)pd - KERNEL_BASE) | 0x3ULL;
+        pt = kernel_virt_end_aligned;
+        for (i = 0; i < total_pde; i++)
+        {
+            pd[i] = ((uint64_t)pt + (i * 4096) - KERNEL_BASE) | 0x3ULL;
+        }
+        for (i = 0; i < total_pte; i++)
+        {
+            pt[i] = (i * 0x1000ULL) | 0x3ULL;
+            kernel_virt_end_aligned++;
+        }
     }
 
-    pt = kernel_virt_end_aligned;
-    kernel_virt_end_aligned = (uint64_t *)ALIGN_4K(kernel_virt_end_aligned + total_pte);
+    load_pml4((uint64_t)pml4 - KERNEL_BASE);
     asm volatile(
         "movq %0, %%rax\r\n"
         "movq %1, %%rbx\r\n"
         "movq %2, %%rcx\r\n"
         "movq %3, %%rdx\r\n"
         "movq %4, %%r8\r\n"
-        "hlt\r\n"
         :
         : "r"(pml4), "r"(pdpt), "r"(pd), "r"(pt), "r"(kernel_virt_end_aligned)
         : "rax", "rbx", "rcx", "rdx", "r8");
+}
+
+void load_pml4(uint64_t pml4_physaddr)
+{
+    asm volatile(
+        "movq %0, %%rax\r\n"
+        "movq %%rax, %%cr3\r\n"
+        :
+        : "r"(pml4_physaddr)
+        : "rax");
 }
